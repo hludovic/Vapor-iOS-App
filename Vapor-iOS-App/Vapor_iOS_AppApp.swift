8b58cd01d78f762fc3189609b96b0ca8755dca2f
Ludovic HENRY <badsteam@hotmail.com>
2023-07-15 11:02:33 -0400

    Initial Commit

---
+++ Vapor-iOS-App/Vapor_iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vapor_iOS_AppApp.swift
+//  Vapor-iOS-App
+//
+//  Created by Ludovic HENRY on 15/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Vapor_iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
